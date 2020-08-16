bbbcac01f2106f01da35b5dcffbd99db20b321a7
StephanieJolianis <stephaniejolianis@gmail.com>
2020-08-16 10:58:59 -0500

    view3 mobile

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -2,6 +2,7 @@ import viewLogin from "../views/login.html";
 import logoImg from "../img/logo.png";
 import faceImg from "../img/facebook.png";
 import googleImg from "../img/google.png";
+import arrowBack from "../img/arrow-left.png"
 
 export default () => {
     const divElement = document.createElement("main");
@@ -16,6 +17,9 @@ export default () => {
 
     const imgGoogle = divElement.querySelector("#googleButton");
     imgGoogle.src = googleImg;
+
+    const imgBackBotton = divElement.querySelector("#back");
+    imgBackBotton.src = arrowBack;
 
 
     return divElement;
--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -1,9 +1,26 @@
 import viewSignUp from "../views/signup.html";
+import logoView3 from "../img/logo.png";
+import faceImg from "../img/facebook.png";
+import googleImg from "../img/google.png";
+import arrowBack from "../img/arrow-left.png"
 
 export default () => {
     const divElement = document.createElement("main");
     divElement.classList = "mainView3";
     divElement.innerHTML = viewSignUp;
+
+    const imgLogoView3 = divElement.querySelector("#logoView3");
+    imgLogoView3.src = logoView3;
+
+    const imgFace = divElement.querySelector("#faceButton");
+    imgFace.src = faceImg;
+
+    const imgGoogle = divElement.querySelector("#googleButton");
+    imgGoogle.src = googleImg;
+
+    const imgBackBotton = divElement.querySelector("#back");
+    imgBackBotton.src = arrowBack;
+
 
     return divElement;
 }
