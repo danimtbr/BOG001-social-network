61f7fdb3fdc4831d4d26c0632d8ee056662f8185
Daniela Mantilla Barragán <damantillab@gmail.com>
2020-08-15 20:52:43 -0500

    segunda vista mobile

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,8 +1,22 @@
 import viewLogin from "../views/login.html";
-
+import logoImg from "../img/logo.png";
+import faceImg from "../img/facebook.png";
+import googleImg from "../img/google.png";
 
 export default () => {
     const divElement = document.createElement("main");
     divElement.classList = "mainView2";
     divElement.innerHTML = viewLogin;
+
+    const imgLogo = divElement.querySelector("#logoView2");
+    imgLogo.src = logoImg;
+
+    const imgFace = divElement.querySelector("#faceButton");
+    imgFace.src = faceImg;
+
+    const imgGoogle = divElement.querySelector("#googleButton");
+    imgGoogle.src = googleImg;
+
+
+    return divElement;
 }
