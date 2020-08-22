b7dca9783c3adf1d4e600946397dcfb27041d5d2
Daniela Mantilla Barragán <damantillab@gmail.com>
2020-08-22 14:05:35 -0500

    Icreación de la estructura de la data y función de impresión de los post

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,8 +1,10 @@
 import viewPosts from "../views/posts.html";
+import collectionPost from "../views/postcollection.html";
 
 export default () => {
 
     const divElement = document.createElement("main");
+    divElement.id = "containerPost";
     divElement.classList = "mainViewPosts";
     divElement.innerHTML = viewPosts;
 
@@ -14,23 +16,47 @@ export default () => {
         })
     })
 
-
     const postForm = divElement.querySelector("#formPost");
 
     const createPost = async(post) => {
         await database.collection("posts").doc().set({
-            post
+            "userId": post.userId,
+            "postDate": post.postDate,
+            "postText": post.postText
         })
+    }
+
+    let objectPost = {
+        UserId: "sofi",
+        PostDate: Date.now(),
+        PostText: ""
     }
 
     postForm.addEventListener("submit", async event => {
         event.preventDefault();
 
-        const postText = postForm["textPost"].value;
+        const postText = postForm["textPost"];
+        objectPost.postText = postText.value;
+        await createPost(objectPost);
+        postForm.reset();
+        postForm.focus();
 
-        await createPost(postText);
-
-    })
-
+    });
+    const getPost = async() => await database.collection("posts").get();
+    window.addEventListener("DOMContentLoaded", async event => {
+        event.preventDefault();
+        const querySnapshot = await getPost();
+        querySnapshot.forEach(element => {
+            console.log(element.data());
+            const dataElement = element.data();
+            const divCollection = document.createElement("div");
+            divCollection.classList = "divPost";
+            divCollection.innerHTML = collectionPost;
+            let postCollection = divCollection.querySelector("#textPost");
+            postCollection.innerHTML = dataElement.postText;
+            divElement.appendChild(divCollection);
+            console.log(divElement)
+        });
+    });
     return divElement;
 }
